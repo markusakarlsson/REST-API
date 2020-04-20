b982ebeae9deb2c3cbd263e9e66ba1b2d0ea1e48
Markus Karlsson <markuskarlsson__@hotmail.com>
2020-04-20 17:38:12 +0200

    Fixing PUT and POST

--- server.js
+++ server.js
@@ -10,33 +10,31 @@ const players = [
         name: "Henrik Lundqvist",
         age:  38,
         position: "Goalkeeper",
-        team: "New York Rangers",
+        team: "New York Rangers"
     }, {
         id: 2,
         name: "Peter Forsberg",
         age:  46,
         position: "Center",
-        team: "Colorado Avalanche",
+        team: "Retired"
     }, {
         id: 3,
         name: "Elias Pettersson",
         age:  21,
         position: "Center",
-        team: "Vancouver Canucks",
+        team: "Vancouver Canucks"
     }, {
         id: 4,
         name: "Mats Sundin",
         age: 49,
         position: "Center",
-        team: "Retired",
+        team: "Retired"
     }
 ]
 
-
-
 // GET
 app.get('/', (req, res) => {
-    res.send('Welcome to the hockeyplayers database')
+    res.send('Welcome to the Hockeyplayer Database')
 })
 
 // GET all players
@@ -61,23 +59,33 @@ app.get('/players/:playerId', (req, res) => {
     }
 })
 
+// app.use(express.json())
+
 // POST Add a player
 app.post('/players', (req, res) => {
+    const player = {
+        id: req.body.id,
+        name: req.body.name,
+        age: req.body.age,
+        position: req.body.position,
+        team: req.body.team,
+    }
+
+    if(!player) {
+        res.status(404).send()
+    } else {
+        players.push(req.body).json
+        res.status(201).send(player)
+    }
+    
     // if(!req.body.name || !req.body.age || !req.body.position || !req.body.team)
     //     return res.status(400).send('Name, age, position and team is required.')
 
-    // const player = {
-    //     id: players.length + 1,
-    //     name: req.body.name,
-    //     age: req.body.age,
-    //     position: req.body.position,
-    //     team: req.body.team,
-    // }
 
     // players.push(player)
     // res.status(201).json(null)
-    players.push(req.body).json
-    res.status(201).json(null)
+    // players.push(req.body).json
+    // res.status(201).json(null)
 })
 
 // PUT Update a specific player
@@ -91,12 +99,12 @@ app.put('/players/:playerId', (req, res) => {
     })
 
     if(!specificPlayer) {
-        res.status(404).json('No player found on the given ID')
+        res.status(404).send('No player found on the given ID')
     } else {
-        player.name = req.body.name,
-        player.age = req.body.age,
-        player.position = req.body.position,
-        player.team = req.body.team,
+        specificPlayer.name = req.body.name,
+        specificPlayer.age = req.body.age,
+        specificPlayer.position = req.body.position,
+        specificPlayer.team = req.body.team,
         res.send(specificPlayer)
     }
 })
